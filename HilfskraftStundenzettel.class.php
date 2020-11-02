b21798794c78eaee6215e8e553d3af1b1d6e206f
Annelene Sudau <sudau@elan-ev.de>
2020-11-02 18:52:53 +0100

    basic plugin files

---
+++ HilfskraftStundenzettel.class.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * HilfskraftStundenzettel.class.php
+ *
+ * ...
+ *
+ * @author  Annelene Sudau <asudau@uos.de>
+ * @version 1.0
+ */
+
+
+class HilfskraftStundenzettel extends StudipPlugin implements SystemPlugin
+{
+
+    const STUNDENVERWALTUNG_ROLE = 'Hilfskraft-Stundenzettelverwaltung';
+    
+    public function __construct()
+    {
+        parent::__construct();
+        global $perm;
+
+        if(RolePersistence::isAssignedRole($GLOBALS['user']->user_id,
+                                                            self::STUNDENVERWALTUNG_ROLE)){
+            $navigation = new Navigation('Hilfskraft-Stundenzettelverwaltung');
+            $navigation->setImage(Icon::create('edit', 'navigation'));
+            $navigation->setURL(PluginEngine::getURL($this, array(), 'index'));
+            
+            //$item = new Navigation(_('Übersicht'), PluginEngine::getURL($this, array(), 'index'));
+            //$navigation->addSubNavigation('index', $item);
+            
+            $item = new Navigation(_('Studentische MitarbeiterInnen'), PluginEngine::getURL($this, array(), 'index/members'));
+            $navigation->addSubNavigation('members', $item);
+            
+            Navigation::addItem('tools/hilfskraft-stundenverwaltung', $navigation);  
+        }    
+    }
+
+    public function initialize ()
+    {
+        
+    }
+
+    public function perform($unconsumed_path)
+    {
+        $this->setupAutoload();
+        $dispatcher = new Trails_Dispatcher(
+            $this->getPluginPath(),
+            rtrim(PluginEngine::getLink($this, array(), null), '/'),
+            'show'
+        );
+        $dispatcher->plugin = $this;
+        $dispatcher->dispatch($unconsumed_path);
+        
+    }
+
+    private function setupAutoload()
+    {
+        if (class_exists('StudipAutoloader')) {
+            StudipAutoloader::addAutoloadPath(__DIR__ . '/models');
+        } else {
+            spl_autoload_register(function ($class) {
+                include_once __DIR__ . $class . '.php';
+            });
+        }
+    }
+}
---
+++ controllers/index.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__ . '/../models/...class.php';
+
+class IndexController extends StudipController {
+
+    public function __construct($dispatcher)
+    {
+        parent::__construct($dispatcher);
+        $this->plugin = $dispatcher->plugin;
+
+    }
+
+    public function before_filter(&$action, &$args)
+    {
+        parent::before_filter($action, $args);
+        PageLayout::setTitle(_("Studentische MitarbeiterInnen - Übersicht"));
+
+
+    }
+
+    public function index_action($selection = NULL)
+    {
+        Navigation::activateItem('tools/hilfskraft-stundenverwaltung/index');
+        $views = new ViewsWidget();
+        $views->addLink(_('Übersicht über Studentische MitarbeiterInnen'),
+                        $this->url_for('index'))
+              ->setActive($action === 'index');
+
+        Sidebar::get()->addWidget($views);
+
+    }
+    
+    // customized #url_for for plugins
+    public function url_for($to = '')
+    {
+        $args = func_get_args();
+
+        # find params
+        $params = array();
+        if (is_array(end($args))) {
+            $params = array_pop($args);
+        }
+
+        # urlencode all but the first argument
+        $args = array_map('urlencode', $args);
+        $args[0] = $to;
+
+        return PluginEngine::getURL($this->dispatcher->plugin, $params, join('/', $args));
+    }
+    
+}
