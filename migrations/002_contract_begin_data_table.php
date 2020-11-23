0a985124614498014a3aa021e7ec2f769e5ef879
asudau <sudau@elan-ev.de>
2020-11-23 22:32:33 +0100

    rel #33

---
+++ migrations/002_contract_begin_data_table.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/../constants.inc.php';
+
+class ContractBeginDataTable extends Migration
+{
+    public function description()
+    {
+        return 'table for contract-begin-data ';
+    }
+
+    public function up()
+    {
+        
+        $db = DBManager::get();
+        //add db-table 
+        $db->exec("CREATE TABLE IF NOT EXISTS `stundenzettel_contract_begin` (
+            `contract_id` varchar(32) NOT NULL,
+            `balance` varchar(6) NOT NULL,
+            `vacation_claimed` varchar(6) NOT NULL,
+            `begin_digital_recording_month` int(2) NOT NULL,
+            `begin_digital_recording_year` int(4) NOT NULL,
+            PRIMARY KEY (id)
+        ) ");
+        
+        SimpleORMap::expireTableScheme();
+    }
+
+    public function down()
+    {
+        $db = DBManager::get();
+
+        $db->exec("DROP TABLE stundenzettel_contract_begin");
+
+        SimpleORMap::expireTableScheme();
+    }
+}
+
