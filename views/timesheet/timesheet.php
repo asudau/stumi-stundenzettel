6de6f66f24ee1e3b16667618e688d1e4aeb61d89
asudau <sudau@elan-ev.de>
2020-12-09 16:17:25 +0100

    fix #46

--- views/timesheet/timesheet.php
+++ views/timesheet/timesheet.php
@@ -7,10 +7,10 @@ use Studip\Button, Studip\LinkButton;
 </div>
 
 <?php if ($no_timesheet) : ?>
-    <h3>Name, Vorname der Hilfskraft: <?= User::findOneByUser_Id($contract->stumi_id)->nachname ?>, <?= User::findOneByUser_Id($contract->stumi_id)->vorname ?></h3>
-    <h3>Fachbereich/Organisationseinheit: <?= Institute::find($contract->inst_id)->name ?></h3>
+    <p>Name, Vorname: <b><?= User::findOneByUser_Id($contract->stumi_id)->nachname ?>, <?= User::findOneByUser_Id($contract->stumi_id)->vorname ?></b></p>
+    <p>Fachbereich/Organisationseinheit: <b><?= Institute::find($contract->inst_id)->name ?></b></p>
 
-    <h3>Monat/Jahr: 
+    <p>Monat/Jahr: 
         <form name="month_select" method="post"  action="<?= $controller->url_for('timesheet/select/' . $contract->id) ?>">
             <select name ='month' onchange="this.form.submit()">
                 <?php foreach ($plugin->getMonths() as $entry_value): ?>
@@ -23,14 +23,14 @@ use Studip\Button, Studip\LinkButton;
                 <?php endforeach ?>
             </select>
         </form>
-        
-    </h3>
+    </p>
+    
 <?php else : ?>
-
-    <h3>Name, Vorname der Hilfskraft: <?= User::findOneByUser_Id($timesheet->stumi_id)->nachname ?>, <?= User::findOneByUser_Id($timesheet->stumi_id)->vorname ?></h3>
-    <h3>Fachbereich/Organisationseinheit: <?= Institute::find($inst_id)->name ?></h3>
-    <h3>Monatsarbeitszeit laut Arbeitsvertrag: <?= $timesheet->contract->contract_hours ?> Stunden</h3>
-    <h3>Monat/Jahr: 
+    
+    <p>Name, Vorname: <b><?= User::findOneByUser_Id($timesheet->stumi_id)->nachname ?>, <?= User::findOneByUser_Id($timesheet->stumi_id)->vorname ?></b></p>
+    <p>Fachbereich/Organisationseinheit: <b><?= Institute::find($inst_id)->name ?></b></p>
+    <p>Monatsarbeitszeit laut Arbeitsvertrag: <b><?= $timesheet->contract->contract_hours ?> Stunden </b></p>
+    <p>Monat/Jahr: 
         <form name="month_select" method="post"  action="<?= $controller->url_for('timesheet/select/' . $timesheet->contract_id) ?>">
             <select name ='month' onchange="this.form.submit()">
                 <?php foreach ($plugin->getMonths() as $entry_value): ?>
@@ -43,8 +43,8 @@ use Studip\Button, Studip\LinkButton;
                 <?php endforeach ?>
             </select>
         </form>
-        
-    </h3>
+    </p>
+
 
 <form id="timesheet_form" method="post" class='<?= $adminrole ? 'admin' : '' ?> <?= $timesheet->locked ? 'locked' : '' ?>' onsubmit="return validateForm()" action="<?= $controller->url_for('timesheet/save_timesheet', $timesheet->id) ?>" class="default collapsable">
     <?= CSRFProtection::tokenTag() ?>
