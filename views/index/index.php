b92b3be5a568b3acca482020ead7785c5c8d9d71
asudau <sudau@elan-ev.de>
2021-07-26 12:50:48 +0200

    rel #151 and some cleanup

--- views/index/index.php
+++ views/index/index.php
@@ -14,15 +14,14 @@
                     <th data-sort="text" style='width:10%'>Nachname, Vorname</th>
                     <th data-sort="htmldata" style='width:10%'>Vertrags- <br>beginn</th>
                     <th data-sort="htmldata" style='width:10%'>Vertrags- <br>ende</th>
-                    <th data-sort="digit" style='width:10%'>Monats- </br>stunden</th>
-                    <th data-sort="digit" style='width:5%'>Tagessatz</th>
-                    <th data-sort="digit" style='width:10%'>Stundenkonto</br>(exkl. <?= strftime('%B') ?>)</th>
+                    <th data-sort="digit" style='width:7%'>Monats- </br>stunden/ </br> Tagessatz</th>
+                    <th data-sort="digit" style='width:7%'>Stundenkonto</br>(exkl. <?= strftime('%B') ?>)</th>
                     <th data-sort="false" style='width:10%'>Urlaub in Anspruch genommen <?= date('Y') ?></th>
-                    <th data-sort="digit" style='width:10%'>Resturlaub</br><?= date('Y') ?></th>
+                    <th data-sort="digit" style='width:7%'>Resturlaub</br><?= date('Y') ?></th>
                     <th data-sort="false" style='width:10%'>Urlaubsanspruch</br><?= date('Y') ?></th>
                     <th data-sort="digit" style='width:10%'>Resturlaub</br><?= date('Y')-1 ?></br>zu Jahresbeginn</th>
                     <th data-sort="false" style='width:10%'>Verantwortliche/r MA</th>
-                    <th>Aktionen</th>
+                    <th style='width:10%'>Aktionen</th>
                 </tr>
             </thead>
             <tbody>
@@ -30,7 +29,7 @@
                 <? if ($inst_data[$inst_id]->stumi_contracts[$stumi->user_id]) : ?>
                     <? foreach ($inst_data[$inst_id]->stumi_contracts[$stumi->user_id] as $contract): ?>
                     <tr>  
-                        <td><a href='<?=$this->controller->link_for('timesheet/index/' . $contract->id) ?>' title='Stundenzettel einsehen'><?= htmlready($stumi->nachname) ?>, <?= htmlready($stumi->vorname) ?></a>
+                        <td title='Kennung: <?= $stumi->username ?>'><a href='<?=$this->controller->link_for('timesheet/index/' . $contract->id) ?>' ><?= htmlready($stumi->nachname) ?>, <?= htmlready($stumi->vorname) ?></a>
                         </td>
                         <td data-sort-value="<?= $contract->contract_begin ?>"><?= date('d.m.Y', $contract->contract_begin) ?>
                             <? if ($contract->begin_digital_recording_month && $contract->begin_digital_recording_year) : ?>
@@ -39,14 +38,13 @@
                             <? endif ?>
                         </td>
                         <td data-sort-value="<?= $contract->contract_end ?>"><?= date('d.m.Y', $contract->contract_end) ?></td>
-                        <td><?= htmlready($contract->contract_hours) ?></td>
-                        <td><?= htmlready($contract->default_workday_time) ?></td>
+                        <td><?= htmlready($contract->contract_hours) ?> / <?= htmlready($contract->default_workday_time) ?></td>
                         <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getWorktimeBalance())) ?></td>
                         <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getClaimedVacation(date('Y'))))?></td>
                         <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getRemainingVacation(date('Y'))))?></td>
                         <td><?= htmlready($contract->getVacationEntitlement(date('Y')))?></td>
                         <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getRemainingVacationAtEndOfYear(date('Y')-1)))?></td>
-                        <td><?= htmlready(User::findOneByUser_Id($contract->supervisor)->username) ?></td>
+                        <td title='<?= User::find($contract->supervisor)->username ?>'><?= sprintf('%s %s', User::find($contract->supervisor)->vorname, User::find($contract->supervisor)->nachname) ?></td>
                         <td>
                             <? if ($adminrole) : ?>
                                 <a data-confirm='Eintrag löschen?' href='<?=$this->controller->link_for('index/delete/' . $contract->id) ?>' title='Eintrag löschen' ><?=Icon::create('trash')?></a>
@@ -63,7 +61,6 @@
                     <tr> 
                         <td><?= htmlready($stumi->nachname) ?>, <?= htmlready($stumi->vorname) ?>
                         </td>
-                        <td> -- </td>
                         <td> -- </td>
                         <td> -- </td>
                         <td> -- </td>
@@ -93,11 +90,10 @@
                     <th data-sort="text" style='width:10%'>Nachname, Vorname</th>
                     <th data-sort="htmldata" style='width:10%'>Vertrags- <br>beginn</th>
                     <th data-sort="htmldata" style='width:10%'>Vertrags- <br>ende</th>
-                    <th data-sort="digit" style='width:10%'>Monats- </br>stunden</th>
-                    <th data-sort="digit" style='width:5%'>Tagessatz</th>
-                    <th data-sort="digit" style='width:10%'>Stundenkonto</br>(exkl. <?= strftime('%B') ?>)</th>
+                    <th data-sort="digit" style='width:7%'>Monats- </br>stunden/ </br>Tagessatz</th>
+                    <th data-sort="digit" style='width:7%'>Stundenkonto</br>(exkl. <?= strftime('%B') ?>)</th>
                     <th data-sort="false" style='width:10%'>Urlaub in Anspruch genommen <?= date('Y') ?></th>
-                    <th data-sort="digit" style='width:10%'>Resturlaub</br><?= date('Y') ?></th>
+                    <th data-sort="digit" style='width:7%'>Resturlaub</br><?= date('Y') ?></th>
                     <th data-sort="false" style='width:10%'>Urlaubsanspruch</br><?= date('Y') ?></th>
                     <th data-sort="digit" style='width:10%'>Resturlaub</br><?= date('Y')-1 ?></br>zu Jahresbeginn</th>
                 </tr>
@@ -115,8 +111,7 @@
                         <? endif ?>
                     </td>
                     <td data-sort-value="<?= $contract->contract_end ?>"><?= date('d.m.Y', $contract->contract_end) ?></td>
-                    <td><?= htmlready($contract->contract_hours) ?></td>
-                    <td><?= htmlready($contract->default_workday_time) ?></td>
+                    <td><?= htmlready($contract->contract_hours) ?> / <?= htmlready($contract->default_workday_time) ?></td>
                     <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getWorktimeBalance())) ?></td>
                     <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getClaimedVacation(date('Y'))))?></td>
                     <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getRemainingVacation(date('Y'))))?></td>
@@ -142,18 +137,31 @@
                 <th data-sort="false" style='width:10%'>Stundenkonto (exkl. <?= strftime('%B') ?>)</th>
 <!--                <th data-sort="false" style='width:10%'>Resturlaub/Urlaubsanspruch <?= date('Y') ?></th>                -->
                 <th data-sort="false" style='width:10%'>Verantwortliche/r MA</th>
+                <th data-sort="false" style='width:10%'>Aktionen</th>
             </tr>
         </thead>
         <tbody>
             <? foreach ($stumi_contracts as $contract): ?>
                 <tr>
                     <td><?= htmlready(Institute::find($contract->inst_id)->name) ?></td>
-                    <td><?= date('d.m.Y', $contract->contract_begin) ?></td>
+                    <td><?= date('d.m.Y', $contract->contract_begin) ?>
+                        <? if ($contract->begin_digital_recording_month && $contract->begin_digital_recording_year) : ?>
+                                <?= Icon::create('info-circle', Icon::ROLE_CLICKABLE,  
+                                        ['title' => 'Beginn der elektronischen Erfassung: ' . $contract->begin_digital_recording_month . '/' . $contract->begin_digital_recording_year ]);?>
+                        <? endif ?>
+                    </td>
                     <td><?= date('d.m.Y', $contract->contract_end) ?></td>
                     <td><?= htmlready($contract->contract_hours) ?></td>
                     <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getWorktimeBalance())) ?></td>
 <!--                    <td><?= htmlready(StundenzettelTimesheet::stundenzettel_strftimespan($contract->getRemainingVacation(date('Y')))) ?>/<?= htmlready($contract->getVacationEntitlement(date('Y'))) ?></td>-->
-                    <td><?= htmlready(User::findOneByUser_Id($contract->supervisor)->username) ?></td>
+                    <td title='<?= User::find($contract->supervisor)->username ?>'><?= sprintf('%s %s', User::find($contract->supervisor)->vorname, User::find($contract->supervisor)->nachname) ?></td>
+                    <td>
+                        <a data-dialog="title='eMail an verantwortliche/n MA';size=1000x800;" 
+                            href="<?=$this->controller->link_for('index/mail/' . $contract->supervisor)?>" 
+                            title="eMail an verantwortliche/n MA">
+                            <?= Icon::create('mail') ?>
+                        </a>
+                    </td>
                 </tr>
             <? endforeach ?>
         </tbody>
